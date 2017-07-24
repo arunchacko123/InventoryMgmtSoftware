129caa0f20580777280c18b867ca8a00f3673010
arun <arun@MJ99.mjsindia.com>
2017-07-24 21:06:01 +0530

    model for Category & SubCategory

--- ims/src/main/java/com/inventory/mgmt/ims/model/Category.java
+++ ims/src/main/java/com/inventory/mgmt/ims/model/Category.java
@@ -1,5 +1,83 @@
 package com.inventory.mgmt.ims.model;
 
-public class Category {
+import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+@Entity
+@Table(name="category")
+public class Category implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "category_id")
+	private Long categoryId;
+	@NotEmpty
+	@Column(name = "category_code")
+	private Long categoryCode;
+	@NotEmpty
+	@Column(name="category_name", nullable=false)
+	private String categoryName;
+	@NotEmpty
+	@Column(name="category_description", nullable=false)
+	private String categoryDesc;
+	@NotEmpty
+	@Column(name="isactive ", nullable=false)
+	private boolean isActive;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+	private Set<Subcategory> subCategory;
+	
+	public Long getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+	public Long getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(Long categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getCategoryDesc() {
+		return categoryDesc;
+	}
+	public void setCategoryDesc(String categoryDesc) {
+		this.categoryDesc = categoryDesc;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public Set<Subcategory> getSubCategory() {
+		return subCategory;
+	}
+	public void setSubCategory(Set<Subcategory> subCategory) {
+		this.subCategory = subCategory;
+	}
+	
+	
+	
+	
+	
 }
--- ims/src/main/java/com/inventory/mgmt/ims/model/Subcategory.java
+++ ims/src/main/java/com/inventory/mgmt/ims/model/Subcategory.java
@@ -1,5 +1,77 @@
 package com.inventory.mgmt.ims.model;
 
-public class Subcategory {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name="sub_category")
+public class Subcategory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "sub_category_id ")
+	private Long subCategoryId;
+	@NotEmpty
+	@Column(name = "sub_category_code")
+	private Long subCategoryCode;
+	@NotEmpty
+	@Column(name="sub_category_name ", nullable=false)
+	private String subCategoryName;
+	@NotEmpty
+	@Column(name="sub_category_description", nullable=false)
+	private String subCategoryDesc;
+	@NotEmpty
+	@Column(name="isactive ", nullable=false)
+	private boolean isActive;
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+	
+	
+	public Long getSubCategoryId() {
+		return subCategoryId;
+	}
+	public void setSubCategoryId(Long subCategoryId) {
+		this.subCategoryId = subCategoryId;
+	}
+	public Long getSubCategoryCode() {
+		return subCategoryCode;
+	}
+	public void setSubCategoryCode(Long subCategoryCode) {
+		this.subCategoryCode = subCategoryCode;
+	}
+	public String getSubCategoryName() {
+		return subCategoryName;
+	}
+	public void setSubCategoryName(String subCategoryName) {
+		this.subCategoryName = subCategoryName;
+	}
+	public String getSubCategoryDesc() {
+		return subCategoryDesc;
+	}
+	public void setSubCategoryDesc(String subCategoryDesc) {
+		this.subCategoryDesc = subCategoryDesc;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+   
 }

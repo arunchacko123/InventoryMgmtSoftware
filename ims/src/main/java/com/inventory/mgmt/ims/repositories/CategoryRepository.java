ad60ab2f04008ecc20b60c4a1794552ac1cde445
sujithlalkc <sujithlalkc@gmail.com>
2017-08-02 20:39:38 +0530

    Category property not found issue solved

--- ims/src/main/java/com/inventory/mgmt/ims/repositories/CategoryRepository.java
+++ ims/src/main/java/com/inventory/mgmt/ims/repositories/CategoryRepository.java
@@ -3,7 +3,6 @@ package com.inventory.mgmt.ims.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.inventory.mgmt.ims.model.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
